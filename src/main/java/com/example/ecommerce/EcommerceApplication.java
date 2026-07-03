8a99f53396d7cf2e308f20728eeb07e81cc2528d
Chinwe <chinwenduohabuire16@gmail.com>
2026-07-03 16:28:31 -0400

    chore: added application properties

---
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
