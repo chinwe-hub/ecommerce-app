ec6be121ac9de03824840169f2e09218a12f5ca5
Chinwe <chinwenduohabuire16@gmail.com>
2026-07-08 15:40:14 -0400

    feat: addes more endpoints to product controller

--- src/main/java/com/example/ecommerce/controller/ProductController.java
+++ src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -3,10 +3,12 @@ package com.example.ecommerce.controller;
 import com.example.ecommerce.model.Product;
 import com.example.ecommerce.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
@@ -19,4 +21,30 @@ public class ProductController {
         return productRepository.findAll();
     }
 
+    @PostMapping("/create")
+    public Product createProduct(@RequestBody Product product) {
+        log.debug("product values:{}", product);
+        return productRepository.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product updated) {
+        Product existing = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        log.info("previous existing: {}", existing);
+
+        existing.setName(updated.getName());
+        existing.setDescription(updated.getDescription());
+        existing.setPrice(updated.getPrice());
+        existing.setStockQuantity(updated.getStockQuantity());
+        log.info("updated existing: {}", existing);
+
+        return productRepository.save(existing);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productRepository.deleteById(id);
+    }
 }
