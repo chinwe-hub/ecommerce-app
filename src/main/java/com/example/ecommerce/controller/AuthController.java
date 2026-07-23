66df3f3ca1d6f7bd8681d8cc7475ef7c91bdba61
Chinwe <chinwenduohabuire16@gmail.com>
2026-07-23 19:24:58 -0400

    feat: added jwtauthenticationfilter, customuserdetailsservice,jwtutil and userprincipal class

--- src/main/java/com/example/ecommerce/controller/AuthController.java
+++ src/main/java/com/example/ecommerce/controller/AuthController.java
@@ -4,8 +4,10 @@ import com.example.ecommerce.dto.JwtResponse;
 import com.example.ecommerce.dto.LoginRequest;
 import com.example.ecommerce.dto.RegisterRequest;
 import com.example.ecommerce.model.User;
+import com.example.ecommerce.security.UserPrincipal;
 import com.example.ecommerce.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,7 +23,12 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public JwtResponse login (@RequestBody LoginRequest request) {
+    public JwtResponse login(@RequestBody LoginRequest request) {
         return authService.login(request);
+    }
+
+    @GetMapping("/me")
+    public String getCurrentUser(@AuthenticationPrincipal UserPrincipal principal) {
+        return "You are logged in as: " + principal.getUsername() + " (" + principal.getUser().getRole() + ")";
     }
 }
