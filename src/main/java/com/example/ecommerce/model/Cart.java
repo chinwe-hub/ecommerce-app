fe298ca0d961148955fc611bab60a7f871994360
Chinwe <chinwenduohabuire16@gmail.com>
2026-07-27 12:21:13 -0400

    feat: created cartItemRepository and DTO

---
+++ src/main/java/com/example/ecommerce/model/Cart.java
@@ -0,0 +1,24 @@
+package com.example.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="carts")
+@Data
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", unique = true)
+    private  User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+}
