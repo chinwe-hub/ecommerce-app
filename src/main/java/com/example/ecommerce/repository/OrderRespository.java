71f6782f3b71340e04f0abf80b630cc5945ef867
Chinwe <chinwenduohabuire16@gmail.com>
2026-08-03 18:24:25 -0400

    feat: added  method in order repository

--- src/main/java/com/example/ecommerce/repository/OrderRespository.java
+++ src/main/java/com/example/ecommerce/repository/OrderRespository.java
@@ -1,4 +1,10 @@
 package com.example.ecommerce.repository;
 
-public class OrderRespository {
+import com.example.ecommerce.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRespository extends JpaRepository<Order, Long> {
+    List<Order> findByUserIdOrderByOrderDateDesc(Long userId);
 }
