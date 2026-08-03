c191a6eadbb5466d9744103dd4c484e54fe00eda
Chinwe <chinwenduohabuire16@gmail.com>
2026-08-03 11:58:46 -0400

    feat: added methods in orderitem entity

--- src/main/java/com/example/ecommerce/model/OrderItem.java
+++ src/main/java/com/example/ecommerce/model/OrderItem.java
@@ -1,4 +1,31 @@
 package com.example.ecommerce.model;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+@Data
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    @JsonIgnore
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+
+    private  Integer quantity;
+
+    private BigDecimal price;
+
+
 }
